c081c47116dbe3f1929d3fa750afb98dee99bcd8
Krish Tambe <krish.tambe@gmail.com>
2024-04-30 15:50:45 +0530

    init commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SpringBootApplication
+@RequestMapping("/spring-boot")
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String firstApp(@RequestParam(value = "name",defaultValue = "Coder") String name) {
+		return String.format("Congratulations %s you have built your first spring boot Application from scratch!!", name);
+	}
+	
+
+}
